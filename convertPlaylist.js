7ed52ed97001416343547cb2fdb64e2f41fddb06
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-29 17:27:55 +0000

    created csv reader and converter

--- convertPlaylist.js
+++ convertPlaylist.js
@@ -1,4 +1,35 @@
+// const replay2020 = require('../playlists/replay-2020.csv')
+const csv = require('csv-parser');
+const fs = require('fs');
 
-const convertPlaylist = (file) => {
-  
+// console.log(replay2020)
+
+const convertPlaylist = (rootDirectory) => {
+
+  const playlist = []
+
+    fs.createReadStream(`${rootDirectory}`)
+    .pipe(csv())
+    .on('data', (row) => {
+      storeSongInPlaylist(row);
+    })
+    .on('end', () => {
+      console.log(playlist)
+      console.log('CSV file successfully processed');
+  });
+
+
+  const storeSongInPlaylist = (songObject) => {
+    playlist.push(songObject)
+  }
+
+  return playlist
+
 }
+
+export default convertPlaylist
+
+convertPlaylist('../playlists/replay-2020.csv')
+
+
+
